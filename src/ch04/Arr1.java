0134547e6339b204a068100702b82c1116bdf947
qkrwo <qkrwo@LAPTOP-Q6P4M18G>
2021-03-25 15:37:43 +0900

    2021-03-25

---
+++ src/ch04/Arr1.java
@@ -0,0 +1,14 @@
+package ch04;
+
+public class Arr1 {
+
+	public static void main(String[] args) {
+		int kim = 70, park = 80, choi = 90;
+
+		int[] i1 = new int[5]; //정수 (4byte) 공간 5개 확보, 생성
+		int[] i2 = new int[5];
+		String[] st = new String[5];
+		float[] f1 = new float[10];
+	}
+
+}
---
+++ src/ch04/Arr2.java
@@ -0,0 +1,15 @@
+package ch04;
+
+public class Arr2 {
+
+	public static void main(String[] args) {
+		int i1 = 7;
+		System.out.println("i1= " + i1);
+		int [] i2=new int[5];
+		for (int j = 0; j < 5; j++) {
+			System.out.println(i2[j]);
+		}
+
+	}
+
+}
